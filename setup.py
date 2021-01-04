ad91c7117be530b5e34f9d45edf646892555cdcb
Mike Merrill <mikeam@cs.washington.edu>
2021-01-04 09:34:44 -0800

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='CCode pertaining to the UW Behavioral Data Science Lab's contributions to the Seattle Flu Study ',
+    author='TheMikeMerrill',
+    license='MIT',
+)
