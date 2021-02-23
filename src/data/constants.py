1844cc63fdddd810d875d1d9c3ac277d6e3c17ef
TheMikeMerrill <mikeam@cs.washington.edu>
2021-02-23 10:13:13 -0800

    Constants

---
+++ src/data/constants.py
@@ -0,0 +1,18 @@
+SURVEY_NAMES = ["baseline",
+                "follow_up_a",
+                "follow_up_b",
+                "recovery",
+                "screener"]
+
+MTL_NAMES = [
+    "mtl_lab_order_results",
+    "mtl_lab_order_updates"
+]
+
+PROCESSED_DATASETS = [
+    "lab_results",
+    "lab_updates",
+    "lab_results_with_triggerdate",
+    "baseline_screener_survey",
+    "daily_surveys_onehot"
+]
