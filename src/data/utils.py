2891e5f7631426ea0e7fb6bac1de9a6fea00ed2b
TheMikeMerrill <mikeam@cs.washington.edu>
2021-03-01 14:02:19 -0800

    Utils for working with dask

--- src/data/utils.py
+++ src/data/utils.py
@@ -6,6 +6,13 @@ import pandas as pd
 from src.utils import load_dotenv, get_logger
 import src.data.constants as constants
 
+import multiprocessing.popen_spawn_posix
+
+import dask
+dask.config.set({"distributed.comm.timeouts.connect": "60"})
+
+import dask.dataframe as dd
+
 load_dotenv()
 logger = get_logger()
 
@@ -18,13 +25,14 @@ def get_raw_dataset_path(name):
         return os.path.join(RAW_DATA_PATH,"mtl",name)        
     elif name in constants.SURVEY_NAMES:
         return os.path.join(RAW_DATA_PATH,"survey",name)
+    elif name in constants.ACTIVITY_NAMES:
+        return os.path.join(RAW_DATA_PATH,"activity",name)
     else:
         raise ValueError(f"Looked for {name} in {RAW_DATA_PATH}, not found!")
 
 def find_raw_dataset(name):
     path = get_raw_dataset_path(name)
     return pq.ParquetDataset(path)
-
 
 def load_raw_table(name,fmt="df"):
     dataset = find_raw_dataset(name)
@@ -39,6 +47,8 @@ def load_raw_table(name,fmt="df"):
 def get_processed_dataset_path(name):
     if name in constants.PROCESSED_DATASETS:
         return os.path.join(PROCESSED_DATA_PATH,name+".csv")        
+    elif name in constants.PARQUET_DATASETS:
+        return os.path.join(PROCESSED_DATA_PATH,name) 
     else:
         raise ValueError(f"Looked for {name} in {PROCESSED_DATA_PATH}, not found!")
         
@@ -53,10 +63,24 @@ def load_processed_table(name,fmt="df"):
     dataset = find_processed_dataset(name)
     for column in dataset.columns:
         if "datetime" in str(column):
-            dataset[column] = pd.to_datetime(dataset[column]).dt.tz_convert(None)
-
+            dataset[column] = pd.to_datetime(dataset[column])
     logger.info(f"Reading {name}...")
     if fmt=="df":
         return dataset
     else:
         raise ValueError("Unsupported fmt") 
+
+@dask.delayed
+def get_dask_df(name,min_date=None,max_date=None,index=None):
+    path = get_processed_dataset_path(name)
+    filters = []
+    if min_date:
+        filters.append(["date",">=",min_date])
+    if max_date:
+        filters.append(["date","<",max_date])
+   
+    if filters:
+        df = dd.read_parquet(path,filters=filters,infer_divisions=True)
+    else: 
+        df = dd.read_parquet(path,index=index)
+    return df
