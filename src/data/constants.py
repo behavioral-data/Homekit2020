5f5195b5e9b542fb268e95c05157ed55c0fd5631
TheMikeMerrill <mikeam@cs.washington.edu>
2021-03-01 14:00:48 -0800

    Added minute level activity

--- src/data/constants.py
+++ src/data/constants.py
@@ -9,10 +9,19 @@ MTL_NAMES = [
     "mtl_lab_order_updates"
 ]
 
+ACTIVITY_NAMES = [
+    "fitbit_minute_level_activity"
+]
+
 PROCESSED_DATASETS = [
     "lab_results",
     "lab_updates",
     "lab_results_with_triggerdate",
     "baseline_screener_survey",
-    "daily_surveys_onehot"
+    "daily_surveys_onehot",
 ]
+
+PARQUET_DATASETS = [
+    "processed_fitbit_minute_level_activity"
+]
+
