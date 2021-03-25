7bfdd054efa20201bbca051dfcf44372ef2246f3
TheMikeMerrill <mikeam@cs.washington.edu>
2021-03-25 11:44:16 -0700

    Fixed bug where support wasn't being calculated

--- src/models/eval.py
+++ src/models/eval.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from scipy.special import softmax
 from sklearn.metrics import accuracy_score,precision_recall_fscore_support, roc_auc_score
 from functools import partial
@@ -8,11 +9,13 @@ def classification_eval(logits, labels, threshold = 0.5):
     # Remove indices with pad tokens
     input_probs = softmax(logits, axis=1)
     classes = (input_probs[:, -1] >= threshold)
-    precision, recall, f1, support = precision_recall_fscore_support(labels, classes, average='binary')
+    precision, recall, f1, _ = precision_recall_fscore_support(labels, classes, average='binary')
     results = {}
     results["classification_precision"] = precision
     results["classification_recall"] = recall
     results["classification_f1"] = f1
+    
+    support = pd.Series(labels).value_counts().to_dict()
     results["classification_support"] = support
 
     accuracy =  accuracy_score(labels, classes)
