2afb405f6860fd8c4db46303216377950108e971
TheMikeMerrill <mikeam@cs.washington.edu>
2021-04-04 16:49:54 -0700

    Added train_sand to src

--- src/__main__.py
+++ src/__main__.py
@@ -7,7 +7,7 @@ warnings.filterwarnings("ignore")
 
 import click
 
-from src.models.train_model import train_neural_baseline, train_bert, train_longformer, train_autoencoder
+from src.models.train_model import train_neural_baseline, train_bert, train_longformer, train_autoencoder, train_sand
 from src.utils import get_logger
 logger = get_logger()
 
@@ -18,6 +18,7 @@ cli.add_command(train_neural_baseline)
 cli.add_command(train_bert)
 cli.add_command(train_longformer)
 cli.add_command(train_autoencoder)
+cli.add_command(train_sand)
 
 
 # @cli.command(context_settings=dict(
