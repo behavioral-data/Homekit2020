6f3dabc14cca880ef6627b9fe9e30401e6d95451
TheMikeMerrill <mikeam@cs.washington.edu>
2021-04-06 14:12:57 -0700

    File writing utils

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,10 @@
 import os
-
-import yaml
+import json 
 import logging
+
 import dotenv
+import yaml
+import numpy as np
 
 def load_dotenv():
     project_dir = os.path.join(os.path.dirname(__file__), os.pardir)
@@ -13,6 +15,19 @@ def read_yaml(path):
     with open(path, 'r') as stream:
         return yaml.safe_load(stream)
 
+def clean_datum_for_serialization(datum):
+    for k, v in datum.items():
+        if isinstance(v, (np.ndarray, np.generic)):
+            datum[k] = v.tolist()
+    return datum
+
+def write_jsonl(open_file, data, mode="a"):
+    for datum in data:
+        clean_datum = clean_datum_for_serialization(datum)
+        open_file.write(json.dumps(clean_datum))
+        open_file.write("\n")
+
+
 def get_logger():
     logger = logging.getLogger(__name__)
     logging.basicConfig(
@@ -21,3 +36,10 @@ def get_logger():
         level=logging.INFO
     )
     return logger
+
+def check_for_wandb_run():
+    try:
+        import wandb
+    except ImportError:
+        return None
+    return wandb.run
