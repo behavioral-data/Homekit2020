ba8be5bc5efd821bcd5eca512c92212e801b24f3
TheMikeMerrill <mikeam@cs.washington.edu>
2021-04-08 13:07:25 -0700

    Add CNN Transformer entry point

--- src/__main__.py
+++ src/__main__.py
@@ -7,7 +7,8 @@ warnings.filterwarnings("ignore")
 
 import click
 
-from src.models.train_model import train_neural_baseline, train_bert, train_longformer, train_autoencoder, train_sand
+from src.models.train_model import (train_neural_baseline, train_bert, train_longformer, 
+                                    train_autoencoder, train_sand, train_cnn_transformer)
 from src.models.baselines import select_random
 from src.utils import get_logger
 logger = get_logger()
@@ -21,31 +22,11 @@ cli.add_command(train_bert)
 cli.add_command(train_longformer)
 cli.add_command(train_autoencoder)
 cli.add_command(train_sand)
+cli.add_command(train_cnn_transformer)
 
 cli.add_command(select_random)
 
 
-# @cli.command(context_settings=dict(
-#     ignore_unknown_options=True,
-# ))
-# @click.argument("task_name")
-# @click.argument("model_name")
-# @click.argument('model_args', nargs=-1, type=click.UNPROCESSED)
-# @click.option('--dataset_args')
-# @click.pass_context
-# def train(ctx,task_name,model_name,model_args,dataset_args={}):
-#     logger.info(f"Training {model_name} on {task_name}")
-
-#     dataset_args = loads(dataset_args)
-#     # model_args = process_model_args(model_args)
-
-#     task = get_task_with_name(task_name)(dataset_args=dataset_args)
-#     if model_name in ["cnn","lstm"]:
-#         train_neural_baseline(model_name,task,model_args)
-#     else:
-#         raise NotImplementedError(f"{model_name} not supported")
-
 
 if __name__ == "__main__":
-    print("hey!")
     cli()
