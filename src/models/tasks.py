f05a477b57400f206b6fc061bf611aa821a77eb5
TheMikeMerrill <mikeam@cs.washington.edu>
2021-05-06 10:39:05 -0700

    Added SingleWindowActivityTask

--- src/models/tasks.py
+++ src/models/tasks.py
@@ -1,8 +1,12 @@
 import sys
+import datetime
 
 import src.data.task_datasets as td
 from src.models.eval import classification_eval, autoencode_eval
 from src.data.utils import load_processed_table
+from src.data.cache_datareader import load_cached_activity_reader
+from src.utils import get_logger
+logger = get_logger()
 
 import pandas as pd
 
@@ -28,6 +32,15 @@ class Task(object):
         raise NotImplementedError
     def __str__(self):
         raise NotImplementedError
+    
+    def get_description(self):
+        return self.__doc__
+
+    def get_train_dataset(self):
+        return self.train_dataset
+
+    def get_eval_dataset(self):
+        return self.eval_dataset
 
 class ClassificationMixin():
     def __init__(self):
@@ -70,10 +83,10 @@ class ActivityTask(Task):
         if not split_date:
             raise KeyError("Must provide a date for splitting train and ")
         
-        min_date = dataset_args.pop("min_date",None)
-        max_date = dataset_args.pop("max_date",None)
-        day_window_size = dataset_args.pop("day_window_size",None)
-        max_missing_days_in_window = dataset_args.pop("max_missing_days_in_window",None)
+        min_date = dataset_args.get("min_date",None)
+        max_date = dataset_args.get("max_date",None)
+        day_window_size = dataset_args.get("day_window_size",None)
+        max_missing_days_in_window = dataset_args.get("max_missing_days_in_window",None)
 
         lab_results_reader = td.LabResultsReader()
         participant_ids = lab_results_reader.participant_ids
@@ -83,18 +96,23 @@ class ActivityTask(Task):
         else:
             base_activity_reader = td.DayLevelActivityReader
 
-        minute_level_reader = base_activity_reader(participant_ids=participant_ids,
+        if dataset_args.get("is_cached"):
+            activity_reader = load_cached_activity_reader(self.get_name(),
+                                                          dataset_args=dataset_args,
+                                                          fail_if_mismatched=True)
+        else:
+            activity_reader = base_activity_reader(participant_ids=participant_ids,
                                                            min_date = min_date,
                                                            max_date = max_date,
                                                            day_window_size=day_window_size,
                                                            max_missing_days_in_window=max_missing_days_in_window)
 
-        train_participant_dates, eval_participant_dates = minute_level_reader.split_participant_dates(date=split_date,eval_frac=eval_frac)
+        train_participant_dates, eval_participant_dates = activity_reader.split_participant_dates(date=split_date,eval_frac=eval_frac)
     
 
-        self.train_dataset = base_dataset(minute_level_reader, lab_results_reader,
+        self.train_dataset = base_dataset(activity_reader, lab_results_reader,
                         participant_dates = train_participant_dates,**dataset_args)
-        self.eval_dataset = base_dataset(minute_level_reader, lab_results_reader,
+        self.eval_dataset = base_dataset(activity_reader, lab_results_reader,
                         participant_dates = eval_participant_dates,**dataset_args)
     
     def get_description(self):
@@ -215,6 +233,81 @@ class PredictSurveyCol(Task,ClassificationMixin):
     def get_eval_dataset(self):
         return self.eval_dataset
 
+class SingleWindowActivityTask(Task):
+    """Base class for tasks that make predictions about single windows of data
+       (e.g.) predicting a user's BMI."""
+    
+    def __init__(self,base_dataset,dataset_args={}, activity_level="minute",
+                window_selection="first"):
+        Task.__init__(self)
+        eval_frac = dataset_args.pop("eval_frac",None)
+        split_date = dataset_args.pop("split_date",None)
+
+        min_date = dataset_args.get("min_date",None)
+        max_date = dataset_args.get("max_date",None)
+        day_window_size = dataset_args.get("day_window_size",None)
+        max_missing_days_in_window = dataset_args.get("max_missing_days_in_window",None)
+
+        lab_results_reader = td.LabResultsReader()
+        participant_ids = lab_results_reader.participant_ids
+
+        if activity_level == "minute":
+            base_activity_reader = td.MinuteLevelActivityReader
+        else:
+            base_activity_reader = td.DayLevelActivityReader
+
+        if dataset_args.get("is_cached"):
+            activity_reader = load_cached_activity_reader(self.get_name(),
+                                                          dataset_args=dataset_args,
+                                                          fail_if_mismatched=True)
+        else:
+            activity_reader = base_activity_reader(participant_ids=participant_ids,
+                                                           min_date = min_date,
+                                                           max_date = max_date,
+                                                           day_window_size=day_window_size,
+                                                           max_missing_days_in_window=max_missing_days_in_window)
+
+        train_participant_dates, eval_participant_dates = activity_reader.split_participant_dates(date=split_date,eval_frac=eval_frac)
+        if window_selection == "first":
+            train_participant_dates = self.filter_participant_dates(train_participant_dates)
+            eval_participant_dates = self.filter_participant_dates(eval_participant_dates)
+    
+        else:
+            raise NotImplementedError
+
+
+        self.train_dataset = base_dataset(activity_reader, lab_results_reader,
+                        participant_dates = train_participant_dates,**dataset_args)
+        self.eval_dataset = base_dataset(activity_reader, lab_results_reader,
+                        participant_dates = eval_participant_dates,**dataset_args)
+    
+    def filter_participant_dates(self,participant_dates):
+        candidates = {}
+        for id, date in participant_dates:
+            if candidates.get(id,datetime.datetime.max) > date:
+                candidates[id] = date
+        return list(candidates.items())
+
+class ClassifyObese(SingleWindowActivityTask, ClassificationMixin):
+    def __init__(self, dataset_args, activity_level, window_selection="first"):
+        dataset_args["labeler"] = self.get_labeler()
+        SingleWindowActivityTask.__init__(self, td.CustomLabler, 
+                         dataset_args=dataset_args, 
+                         activity_level=activity_level, 
+                         window_selection=window_selection)
+        ClassificationMixin.__init__(self)      
+
+    def get_labeler(self):
+        self.baseline_results = load_processed_table("baseline_screener_survey")
+        def labeler(participant_id, start_date, end_date):
+            participant_data = self.baseline_results[self.baseline_results["participant_id"]==participant_id].iloc[0]
+            weight = participant_data["weight"]
+            height = (participant_data["height__ft"] *12 + participant_data["height__in"])
+            return weight / (height**2) * 703 > 30.0
+        return labeler
+    
+    def get_name(self):
+        return "ClassifyObese"
 class EarlyDetection(ActivityTask):
     """Mimics the task used by Evidation Health"""
 
@@ -240,15 +333,17 @@ class EarlyDetection(ActivityTask):
         else:
             base_activity_reader = td.DayLevelActivityReader
             
-        minute_level_reader = base_activity_reader(participant_ids=participant_ids,
-                                                            min_date = min_date,
-                                                            max_date = max_date,
-                                                            max_missing_days_in_window=max_missing_days_in_window,
-                                                            day_window_size=day_window_size)
+        if dataset_args.get("is_cached"):
+            logger.info("Loading cached data reader...")
+            activity_reader = load_cached_activity_reader(self.get_name())
+        else:
+            activity_reader = base_activity_reader(participant_ids=participant_ids,
+                                                           min_date = min_date,
+                                                           max_date = max_date,
+                                                           day_window_size=day_window_size,
+                                                           max_missing_days_in_window=max_missing_days_in_window)
 
-        
-        # Maybe there's a cleaner way to do this?
-        
+
         pos_dates = lab_results_reader.results
         pos_dates["timestamp"] = pos_dates["trigger_datetime"].dt.floor("D")
         delta = pd.to_timedelta(window_pad + day_window_size, unit = "days")
@@ -258,7 +353,7 @@ class EarlyDetection(ActivityTask):
         before  = list((pos_dates["timestamp"] -  delta).items())
 
 
-        orig_valid_dates = set(minute_level_reader.participant_dates)
+        orig_valid_dates = set(activity_reader.participant_dates)
         new_valid_dates = list(set(label_date+after+before).intersection(orig_valid_dates))
         
         valid_participants = list(set([x[0] for x in new_valid_dates]))
@@ -272,15 +367,15 @@ class EarlyDetection(ActivityTask):
         train_participant_dates = [x for x in new_valid_dates if x[0] in train_participants]
         eval_participant_dates = [x for x in new_valid_dates if x[0] in eval_participants]
 
-        self.train_dataset = base_dataset(minute_level_reader, lab_results_reader,
+        self.train_dataset = base_dataset(activity_reader, lab_results_reader,
                         participant_dates = train_participant_dates,**dataset_args)
-        self.eval_dataset = base_dataset(minute_level_reader, lab_results_reader,
+        self.eval_dataset = base_dataset(activity_reader, lab_results_reader,
                         participant_dates = eval_participant_dates,**dataset_args)
         
         self.is_classification = True
     
     def get_name(self):
-        return "Early Detection"
+        return "EarlyDetection"
     
     def evaluate_results(self,logits,labels,threshold=0.5):
         return classification_eval(logits,labels,threshold=threshold)
