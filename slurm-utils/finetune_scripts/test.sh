308c2b935a3c9f64289a13a930769f518aca0bd0
TheMikeMerrill <mikeam@cs.washington.edu>
2021-06-10 10:05:40 -0700

    Test script

---
+++ slurm-utils/finetune_scripts/test.sh
@@ -0,0 +1,11 @@
+python ./slurm-utils/slurm-launch.py \
+        --dry-run\
+        --dir "/homes/gws/mikeam/seattleflustudy"\
+        --exp-name "test_1" \
+        --num-nodes 1 \
+        --conda-path "/gscratch/bdata/mikeam/anaconda3/bin/"\
+        --account "bdata" \
+        --partition "bdata-gpu" \
+        --num-gpus 4 \
+        --conda-env seattleflustudy \
+        --command "python ./slurm-utils/raytune-test.py"
