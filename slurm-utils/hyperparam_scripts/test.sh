b040bd22cc7fea9d60cdfc79ae7c60ac21e83c34
TheMikeMerrill <mikeam@cs.washington.edu>
2021-06-11 13:24:07 -0700

    renamed dir

---
+++ slurm-utils/hyperparam_scripts/test.sh
@@ -0,0 +1,11 @@
+python ./slurm-utils/slurm-launch.py \
+        --dry-run\
+        --dir $PWD\
+	--exp-name "test_1" \
+        --num-nodes 1 \
+        --conda-path "/gscratch/bdata/mikeam/anaconda3"\
+        --account "cse" \
+        --partition "cse-gpu" \
+        --num-gpus 6 \
+        --conda-env seattleflustudy \
+        --command "python $1"
