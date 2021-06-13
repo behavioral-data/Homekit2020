1f5e342a39ae8d7c0c8a44c04881698916a76c2b
TheMikeMerrill <mikeam@cs.washington.edu>
2021-06-12 22:45:54 -0700

    Can preload data in cached tasks

--- src/data/cache_resources.py
+++ src/data/cache_resources.py
@@ -1,8 +1,12 @@
 import os
 import pickle
+from src import data
 
 import click
+from pytorch_lightning.trainer import data_loading
 import yaml
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 from src.utils import read_yaml, get_logger
 logger = get_logger(__name__)
@@ -20,9 +24,10 @@ from src.models.tasks import get_task_with_name
 @click.option('--data_location', default=None,type=click.Path(exists=True))
 @click.option('--cache_path', default=None)
 @click.option('--activity_level', default="minute")
+@click.option('--preload', is_flag=True)
 @click.option('--postfix', default="",type=str)
 def main(config_path, task_name=None, cache_path=None, data_location=None, postfix="",
-         activity_level="minute"):
+         activity_level="minute",preload=False):
 
     reader_args = read_yaml(config_path)
     if data_location:
@@ -34,6 +39,13 @@ def main(config_path, task_name=None, cache_path=None, data_location=None, postf
     else:
         resource = get_task_with_name(task_name)(dataset_args=reader_args,
                                         activity_level=activity_level)
+        if preload:
+            train_dataset = resource.get_train_dataset()
+            eval_dataset = resource.get_eval_dataset()
+            for dataset in [train_dataset, eval_dataset]:
+                data_loader = DataLoader(dataset, batch_size=1000)
+
+                [_ for _ in tqdm(data_loader)]
     
     if not cache_path:
         cache_path = get_cached_datareader_path(name)
