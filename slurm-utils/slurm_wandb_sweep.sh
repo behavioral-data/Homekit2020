28be7f06a5f373718a34706b8122076c00f1cc80
Mike Merrill <mikeam@cs.washington.edu>
2021-09-28 17:28:56 -0700

    minor adjustments

--- slurm-utils/slurm_wandb_sweep.sh
+++ slurm-utils/slurm_wandb_sweep.sh
@@ -3,16 +3,18 @@
 #SBATCH --output=slurm_out/%x-%j.log
 #SBATCH --error=slurm_out/%x-%j.out
 #SBATCH --account=bdata
-#SBATCH --partition=gpu-rtx6k
-#SBATCH --time=24:00:00
+#SBATCH --partition=ckpt
+#SBATCH --time=12:00:00
 ### This script works for any number of nodes, Ray will find and manage all resources
 #SBATCH --nodes=1
-#SBATCH --mem=45G
+#SBATCH --mem=90G
 ### Give all resources to a single Ray task, ray can manage the resources internally
 #SBATCH --ntasks-per-node=1
-#SBATCH --gpus-per-task=1
 #SBATCH --chdir=/gscratch/bdata/mikeam/SeattleFluStudy
+#SBATCH --gres=gpu:titan:2
 
+export PATH=$PATH:/gscratch/bdata/mikeam/anaconda3/bin
+source ~/.bashrc
 conda activate seattleflustudy
 wandb agent $1 
 
