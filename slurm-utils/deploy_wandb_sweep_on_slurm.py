3101bdd14189c1ceabf24065363f42f0698b8d5f
Mike Merrill <mikeam@klone1.hyak.uw.edu>
2021-09-28 17:26:23 -0700

    cast to int

--- slurm-utils/deploy_wandb_sweep_on_slurm.py
+++ slurm-utils/deploy_wandb_sweep_on_slurm.py
@@ -5,8 +5,8 @@ import subprocess
 @click.argument("n_jobs")
 @click.argument("sweep_address")
 def main(n_jobs, sweep_address):
-    for i in range(n_jobs):
-        command1 = subprocess.Popen(['srun','slurm-utils/slurm_wandb_sweep.sh', sweep_address])
+    for i in range(int(n_jobs)):
+        command1 = subprocess.Popen(['sbatch','slurm-utils/slurm_wandb_sweep.sh', sweep_address])
 
 if __name__ == "__main__":
     main()
