74175ccbddf8fdd43c1fd794284e66404b03a4d2
TheMikeMerrill <mikeam@cs.washington.edu>
2022-01-11 16:13:02 -0800

    Add early stopping

--- slurm-utils/run_all_tasks.sh
+++ slurm-utils/run_all_tasks.sh
@@ -1,7 +1,7 @@
 TRAIN_PATH="/gscratch/bdata/mikeam/SeattleFluStudy/data/processed/split/audere_split_2020_02_10_to_2020_04_01/train_7_day_no_scale/"
 EVAL_PATH="/gscratch/bdata/mikeam/SeattleFluStudy/data/processed/split/audere_split_2020_02_10_to_2020_04_01/eval_7_day_no_scale/"
 TEST_PATH="/gscratch/bdata/mikeam/SeattleFluStudy/data/processed/split/audere_split_2020_02_10_to_2020_04_01/test_7_day_no_scale/"
-BASE_COMMAND="python src/__main__.py train-cnn-transformer --train_batch_size 700 --dropout_rate=0.4 --focal_gamma=1.7188155817156203 --learning_rate=5e-5 --n_epochs=30 --warmup_steps=20 --val_epochs 2 --model_config model_configs/small_embedding.yaml --train_path $TRAIN_PATH --eval_path $EVAL_PATH --test_path $TEST_PATH"
+BASE_COMMAND="python src/__main__.py train-cnn-transformer --train_batch_size 700 --dropout_rate=0.4 --focal_gamma=1.7188155817156203 --learning_rate=5e-5 --n_epochs=50 --warmup_steps=20 --val_epochs 2 --model_config model_configs/small_embedding.yaml --train_path $TRAIN_PATH --eval_path $EVAL_PATH --test_path $TEST_PATH --early_stopping"
 TASKS=(
     "PredictFluPos"
     "PredictFluSymptoms"
