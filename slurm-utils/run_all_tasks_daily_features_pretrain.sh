6337820dd4f54a1a8bcf096457000118adb57d77
TheMikeMerrill <mikeam@cs.washington.edu>
2022-01-14 20:39:46 -0800

    Features pretrain

---
+++ slurm-utils/run_all_tasks_daily_features_pretrain.sh
@@ -0,0 +1,20 @@
+TRAIN_PATH="/gscratch/bdata/mikeam/SeattleFluStudy/data/processed/split/audere_split_2020_02_10_to_2020_04_01/train_7_day_no_scale/"
+EVAL_PATH="/gscratch/bdata/mikeam/SeattleFluStudy/data/processed/split/audere_split_2020_02_10_to_2020_04_01/eval_7_day_no_scale/"
+TEST_PATH="/gscratch/bdata/mikeam/SeattleFluStudy/data/processed/split/audere_split_2020_02_10_to_2020_04_01/test_7_day_no_scale/"
+PRETRAIN_LOC="/gscratch/bdata/mikeam/SeattleFluStudy/models/pretrained_predict_daily_features.ckpt"
+BASE_COMMAND="python src/__main__.py train-cnn-transformer --train_batch_size 700 --dropout_rate=0.4 --focal_gamma=1.7188155817156203 --learning_rate=5e-5 --n_epochs=50 --warmup_steps=20 --val_epochs 2 --model_config model_configs/small_embedding.yaml --train_path $TRAIN_PATH --eval_path $EVAL_PATH --test_path $TEST_PATH --early_stopping --model_path $PRETRAIN_LOC"
+TASKS=(
+    "PredictFluPos"
+    "PredictFluSymptoms"
+    "PredictMobilityDifficulty"
+    "PredictWeekend"
+    "PredictSevereSymptoms"
+    "PredictCough"
+    "PredictFatigue"
+)
+
+for task in ${TASKS[*]} 
+  do
+    pythonCommand="$BASE_COMMAND --task_config ./src/data/task_configs/$task.yaml"
+    eval "python slurm-utils/launch_on_slurm.py  -n 1 -m '64G' --num-gpus 2 --num-cpus 10 --dir . --exp-name $task --command \"$pythonCommand\""
+  done
