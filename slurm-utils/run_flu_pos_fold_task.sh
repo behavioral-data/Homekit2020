7940ce6dc89ed60dcbe98309c46fb4c22633c344
TheMikeMerrill <mikeam@cs.washington.edu>
2022-02-08 10:59:21 -0800

    freeze encoder

--- slurm-utils/run_flu_pos_fold_task.sh
+++ slurm-utils/run_flu_pos_fold_task.sh
@@ -1,5 +1,5 @@
 PRETRAIN_LOC="/gscratch/bdata/mikeam/SeattleFluStudy/models/pretrained_on_all_flu_neg.ckpt"
-BASE_COMMAND="python src/__main__.py train-cnn-transformer --train_batch_size 700 --dropout_rate=0.4 --learning_rate=5e-4 --n_epochs=300 --warmup_steps=50 --val_epochs 10 --warmup_steps=50  --model_config model_configs/small_embedding.yaml --model_path $PRETRAIN_LOC --task_config ./src/data/task_configs/PredictFluPos.yaml"
+BASE_COMMAND="python src/__main__.py train-cnn-transformer --train_batch_size 700 --dropout_rate=0.4 --learning_rate=5e-4 --freeze_encoder --n_epochs=300 --warmup_steps=50 --val_epochs 10 --model_config model_configs/small_embedding.yaml --model_path $PRETRAIN_LOC --task_config ./src/data/task_configs/PredictFluPos.yaml"
 
 for i in $(seq 0 19); do  
         pythonCommand="$BASE_COMMAND --train_path $PWD/data/processed/flu_pos_folds_20/f${i}_train/ --eval_path $PWD/data/processed/flu_pos_folds_20/f${i}_test/ --notes 'Fold ${i} - Pretrained'"
