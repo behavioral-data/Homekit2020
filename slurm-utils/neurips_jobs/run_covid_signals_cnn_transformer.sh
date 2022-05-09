f95a96528ff37e3e808a2376530ae52a94aa24a7
TheMikeMerrill <mikeam@cs.washington.edu>
2022-05-08 17:38:21 -0700

    Add eval and place config in right place

--- slurm-utils/neurips_jobs/run_covid_signals_cnn_transformer.sh
+++ slurm-utils/neurips_jobs/run_covid_signals_cnn_transformer.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 TRAIN_PATH="/mmfs1/gscratch/bdata/mikeam/MobileSensingSuite/data/processed/COVID_SIGNAL/train_7_day_no_scale_public_only"
 EVAL_PATH="/mmfs1/gscratch/bdata/mikeam/MobileSensingSuite/data/processed/COVID_SIGNAL/eval_7_day_no_scale_public_only"
-BASE_COMMAND="python src/models/train.py fit --config configs/models/CNNToTransformerClassifier.yaml --model.learning_rate 5e-5  --data.train_path $TRAIN_PATH --data.val_path $EVAL_PATH --model.batch_size 800 --trainer.check_val_every_n_epoch 2 --trainer.max_epochs 50 --trainer.log_every_n_steps 50 --early_stopping_patience 2"
+BASE_COMMAND="--config configs/models/CNNToTransformerClassifier.yaml --model.learning_rate 5e-5  --data.train_path $TRAIN_PATH --data.val_path $EVAL_PATH --model.batch_size 800 --trainer.check_val_every_n_epoch 2 --trainer.max_epochs 50 --trainer.log_every_n_steps 50 --early_stopping_patience 2"
 
 TASKS=(
     "CovidSignalsPositivity"
@@ -19,6 +20,6 @@ TASK_ARGS=(
 )
 for i in ${!TASKS[*]}; 
   do
-    pythonCommand="$BASE_COMMAND --config tasks/${TASKS[$i]}.yaml ${TASK_ARGS[$i]}"
-    echo "python slurm-utils/launch_on_slurm.py  -n 1 -m '64G' --num-gpus 2 --num-cpus 10 --dir . --exp-name ${TASKS[$i]} --command \"$pythonCommand\""
+    pythonCommand="python src/models/train.py fit --config tasks/${TASKS[$i]}.yaml $BASE_COMMAND  ${TASK_ARGS[$i]}"
+    eval "python slurm-utils/launch_on_slurm.py  -n 1 -m '64G' --num-gpus 2 --num-cpus 10 --dir . --exp-name ${TASKS[$i]} --command \"$pythonCommand\""
   done
