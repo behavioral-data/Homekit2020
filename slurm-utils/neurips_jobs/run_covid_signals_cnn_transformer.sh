a775f08471dd76b61b74585e2e6aeada2cdc8c74
Mike Merrill <mikeam@cs.washington.edu>
2022-05-18 14:39:22 -0700

    reduce num gpus

--- slurm-utils/neurips_jobs/run_covid_signals_cnn_transformer.sh
+++ slurm-utils/neurips_jobs/run_covid_signals_cnn_transformer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TRAIN_PATH="/mmfs1/gscratch/bdata/mikeam/MobileSensingSuite/data/processed/COVID_Signals/train_7_day_no_scale_public_only"
 EVAL_PATH="/mmfs1/gscratch/bdata/mikeam/MobileSensingSuite/data/processed/COVID_Signals/eval_7_day_no_scale_public_only"
-BASE_COMMAND="--config configs/models/CNNToTransformerClassifier.yaml --model.learning_rate 5e-5  --data.train_path $TRAIN_PATH --data.val_path $EVAL_PATH --model.batch_size 800 --trainer.check_val_every_n_epoch 2 --trainer.max_epochs 50 --trainer.log_every_n_steps 50 --early_stopping_patience 2"
+BASE_COMMAND="--config configs/models/CNNToTransformerClassifier.yaml --model.learning_rate 5e-4  --data.train_path $TRAIN_PATH --data.val_path $EVAL_PATH --model.batch_size 800 --trainer.check_val_every_n_epoch 2 --trainer.max_epochs 200 --trainer.log_every_n_steps 50 --early_stopping_patience 10"
 
 TASKS=(
     "CovidSignalsPositivity"
@@ -21,5 +21,5 @@ TASK_ARGS=(
 for i in ${!TASKS[*]}; 
   do
     pythonCommand="python src/models/train.py fit --config configs/tasks/${TASKS[$i]}.yaml $BASE_COMMAND  ${TASK_ARGS[$i]}"
-    eval "python slurm-utils/launch_on_slurm.py  -n 1 -m '64G' --num-gpus 2 --num-cpus 10 --dir . --exp-name ${TASKS[$i]} --command \"$pythonCommand\""
+    eval "python slurm-utils/launch_on_slurm.py  -n 1 -m '64G' --num-gpus 1 --num-cpus 10 --dir . --exp-name ${TASKS[$i]} --command \"$pythonCommand\""
   done
