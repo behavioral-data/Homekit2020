964d542e51ca5684119eb1f1b0f7ca693aacbeb8
Mike Merrill <mikeam@cs.washington.edu>
2022-05-24 08:53:37 -0700

    Don't need config for resnet

--- slurm-utils/neurips_jobs/run_resnet.sh
+++ slurm-utils/neurips_jobs/run_resnet.sh
@@ -1,6 +1,6 @@
 TRAIN_PATH="/mmfs1/gscratch/bdata/mikeam/MobileSensingSuite/data/processed/COVID_Signals/train_7_day_no_scale_public_only"
 EVAL_PATH="/mmfs1/gscratch/bdata/mikeam/MobileSensingSuite/data/processed/COVID_Signals/eval_7_day_no_scale_public_only"
-BASE_COMMAND="--config configs/models/ResNet.yaml --model ResNet --model.batch_size 64  --model.dropout_rate 0.4  --model.learning_rate 0.003  --data.train_path $TRAIN_PATH --data.val_path $EVAL_PATH --model.batch_size 800 --trainer.check_val_every_n_epoch 1 --trainer.max_epochs 50 --trainer.log_every_n_steps 50 --early_stopping_patience 40 --model.warmup_steps 100"
+BASE_COMMAND="--model ResNet --model.batch_size 64  --model.dropout_rate 0.4  --model.learning_rate 0.003  --data.train_path $TRAIN_PATH --data.val_path $EVAL_PATH --model.batch_size 800 --trainer.check_val_every_n_epoch 1 --trainer.max_epochs 50 --trainer.log_every_n_steps 50 --early_stopping_patience 40 --model.warmup_steps 100"
 
 TASKS=(
     "CovidSignalsPositivity"
