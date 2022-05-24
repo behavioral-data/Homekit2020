2aa808291e1a869a2610a3dc63fa9f9116c9f900
TheMikeMerrill <mikeam@cs.washington.edu>
2022-05-23 19:10:03 -0700

    Update CNN Trans slurm job

--- slurm-utils/neurips_jobs/run_covid_signals_cnn_transformer.sh
+++ slurm-utils/neurips_jobs/run_covid_signals_cnn_transformer.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 TRAIN_PATH="/mmfs1/gscratch/bdata/mikeam/MobileSensingSuite/data/processed/COVID_Signals/train_7_day_no_scale_public_only"
 EVAL_PATH="/mmfs1/gscratch/bdata/mikeam/MobileSensingSuite/data/processed/COVID_Signals/eval_7_day_no_scale_public_only"
-BASE_COMMAND="--config configs/models/CNNToTransformerClassifier.yaml --model.learning_rate 5e-5  --data.train_path $TRAIN_PATH --data.val_path $EVAL_PATH --model.batch_size 800 --trainer.check_val_every_n_epoch 2 --trainer.max_epochs 50 --trainer.log_every_n_steps 50 --early_stopping_patience 2"
+BASE_COMMAND="--config configs/models/CNNToTransformerClassifier.yaml --model.dropout_rate 0.4  --model.learning_rate 0.003  --data.train_path $TRAIN_PATH --data.val_path $EVAL_PATH --model.batch_size 800 --trainer.check_val_every_n_epoch 1 --trainer.max_epochs 50 --trainer.log_every_n_steps 50 --early_stopping_patience 40 --model.warmup_steps 100"
 
 TASKS=(
     "CovidSignalsPositivity"
-    "CovidSignalsPositivity-NoSteps"
     "CovidSignalsAches"
     "CovidSignalsChills"
     "CovidSignalsHeadache"
@@ -13,7 +12,6 @@ TASKS=(
 )
 
 TASK_ARGS=(
-    ""
     ""
     "--data.survey_path data/processed/COVID_Signals/daily_surveys.csv"
     "--data.survey_path data/processed/COVID_Signals/daily_surveys.csv"
@@ -23,5 +21,5 @@ TASK_ARGS=(
 for i in ${!TASKS[*]}; 
   do
     pythonCommand="python src/models/train.py fit --config configs/tasks/${TASKS[$i]}.yaml $BASE_COMMAND  ${TASK_ARGS[$i]}"
-    eval "python slurm-utils/launch_on_slurm.py  -n 1 -m '64G' --num-gpus 2 --num-cpus 10 --dir . --exp-name ${TASKS[$i]} --command \"$pythonCommand\""
+    eval "python slurm-utils/launch_on_slurm.py  -n 1 -m '64G' --num-gpus 1 --num-cpus 5 --dir . --exp-name ${TASKS[$i]} --command \"$pythonCommand\""
   done
