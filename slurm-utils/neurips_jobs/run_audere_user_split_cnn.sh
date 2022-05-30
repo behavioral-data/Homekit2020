182349a9525c8905a9adefc504ab4ff449271a59
TheMikeMerrill <mikeam@cs.washington.edu>
2022-05-30 15:22:50 -0700

    num_hidden layers to zero

---
+++ slurm-utils/neurips_jobs/run_audere_user_split_cnn.sh
@@ -0,0 +1,19 @@
+TRAIN_PATH="/mmfs1/gscratch/bdata/mikeam/MobileSensingSuite/data/processed/audere/audere_split_2020_02_10_to_2020_04_01_by_participant/train_7_day_no_scale"
+EVAL_PATH="/mmfs1/gscratch/bdata/mikeam/MobileSensingSuite/data/processed/audere/audere_split_2020_02_10_to_2020_04_01_by_participant/eval_7_day_no_scale"
+TEST_PATH="/mmfs1/gscratch/bdata/mikeam/MobileSensingSuite/data/processed/audere/audere_split_2020_02_10_to_2020_04_01_by_participant/test_7_day_no_scale"
+BASE_COMMAND="--config configs/models/CNNToTransformerClassifier.yaml --model.num_hidden_layers 0 --model.dropout_rate 0.4  --model.learning_rate 0.003  --data.test_path $TEST_PATH --data.train_path $TRAIN_PATH --data.val_path $EVAL_PATH --model.batch_size 800 --trainer.check_val_every_n_epoch 1 --trainer.max_epochs 50 --trainer.log_every_n_steps 50 --early_stopping_patience 40 --model.warmup_steps 100"
+
+TASKS=(
+    "HomekitPredictFluPos"
+    "HomekitPredictFluSymptoms"
+    "HomekitPredictSevereFever"
+    "HomekitPredictCough"
+    "HomekitPredictFatigue"
+)
+
+
+for i in ${!TASKS[*]}; 
+  do
+    pythonCommand="python src/models/train.py fit --config configs/tasks/${TASKS[$i]}.yaml $BASE_COMMAND --notes 'User Split'"
+    eval "python slurm-utils/launch_on_slurm.py  -n 1 -m '64G' --num-gpus 1 --num-cpus 5 --dir . --exp-name Split-CNNTransformer-${TASKS[$i]} --command \"$pythonCommand\""
+  done
