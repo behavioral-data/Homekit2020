2929255800c98c21d9821a2e2987603c97b08319
TheMikeMerrill <mikeam@cs.washington.edu>
2022-06-02 15:06:44 -0700

    Switch to 2080s

--- slurm-utils/slurm_wandb_sweep.sh
+++ slurm-utils/slurm_wandb_sweep.sh
@@ -3,11 +3,11 @@
 #SBATCH --output=/mmfs1/gscratch/bdata/mikeam/MobileSensingSuite/slurm-utils/jobs/%x-%j.log
 #SBATCH --error=/mmfs1/gscratch/bdata/mikeam/MobileSensingSuite/slurm-utils/jobs/%x-%j.out
 #SBATCH --account=bdata
-#SBATCH --partition=gpu-rtx6k
+#SBATCH --partition=gpu-2080ti
 #SBATCH --time=4:00:00
 #SBATCH --nodes=1
 #SBATCH --mem=45G
-### Give all resources to a single Ray task, ray can manage the resources internally
+#SBATCH --cpus=4
 #SBATCH --ntasks-per-node=1
 #SBATCH --chdir=/gscratch/bdata/mikeam/MobileSensingSuite
 #SBATCH --gres=gpu:1
