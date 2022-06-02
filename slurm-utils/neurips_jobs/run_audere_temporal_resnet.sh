bf5402c9810834dbd114971e8674b6d8d5026ff2
TheMikeMerrill <mikeam@cs.washington.edu>
2022-06-02 13:15:45 -0700

    Didn't have train path!

--- slurm-utils/neurips_jobs/run_audere_temporal_resnet.sh
+++ slurm-utils/neurips_jobs/run_audere_temporal_resnet.sh
@@ -1,7 +1,7 @@
 TRAIN_PATH="/gscratch/bdata/mikeam/SeattleFluStudy/data/processed/split/audere_split_2020_02_10_to_2020_04_01/train_7_day_no_scale/"
 EVAL_PATH="/gscratch/bdata/mikeam/SeattleFluStudy/data/processed/split/audere_split_2020_02_10_to_2020_04_01/eval_7_day_no_scale/"
 TEST_PATH="/gscratch/bdata/mikeam/SeattleFluStudy/data/processed/split/audere_split_2020_02_10_to_2020_04_01/test_7_day_no_scale/"
-BASE_COMMAND="--model src.models.models.ResNet --model.learning_rate 0.003  --data.train_path $TRAIN_PATH --data.val_path $EVAL_PATH --model.batch_size 300 --trainer.check_val_every_n_epoch 1 --trainer.max_epochs 50 --trainer.log_every_n_steps 50 --early_stopping_patience 3 --model.warmup_steps 100"
+BASE_COMMAND="--model src.models.models.ResNet --model.learning_rate 0.003  --data.train_path $TRAIN_PATH --data.val_path $EVAL_PATH  --data.test_path $TEST_PATH --model.batch_size 300 --trainer.check_val_every_n_epoch 1 --trainer.max_epochs 50 --trainer.log_every_n_steps 50 --early_stopping_patience 3 --model.warmup_steps 100"
 
 TASKS=(
     "HomekitPredictFluPos"
