901adb8b56024ecee011b36b27a2ab5da3bdc6e2
TheMikeMerrill <mikeam@cs.washington.edu>
2022-06-02 15:04:47 -0700

    Change slurm wandb to launch one run at a time

--- slurm-utils/deploy_wandb_sweep_on_slurm.py
+++ slurm-utils/deploy_wandb_sweep_on_slurm.py
@@ -6,7 +6,7 @@ import subprocess
 @click.argument("sweep_address")
 def main(n_jobs, sweep_address):
     for i in range(int(n_jobs)):
-        command1 = subprocess.Popen(['sbatch','slurm-utils/slurm_wandb_sweep.sh', sweep_address])
+        command1 = subprocess.Popen(['sbatch','slurm-utils/slurm_wandb_sweep.sh', sweep_address, i])
 
 if __name__ == "__main__":
     main()
--- slurm-utils/slurm_wandb_sweep.sh
+++ slurm-utils/slurm_wandb_sweep.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=wandb_sweep
+#SBATCH --job-name=wandb_sweep_$1_$2
 #SBATCH --output=/mmfs1/gscratch/bdata/mikeam/MobileSensingSuite/slurm-utils/jobs/%x-%j.log
 #SBATCH --error=/mmfs1/gscratch/bdata/mikeam/MobileSensingSuite/slurm-utils/jobs/%x-%j.out
 #SBATCH --account=bdata
 #SBATCH --partition=gpu-rtx6k
-#SBATCH --time=2:00:00
-### This script works for any number of nodes, Ray will find and manage all resources
+#SBATCH --time=4:00:00
 #SBATCH --nodes=1
 #SBATCH --mem=45G
 ### Give all resources to a single Ray task, ray can manage the resources internally
@@ -13,9 +12,6 @@
 #SBATCH --chdir=/gscratch/bdata/mikeam/MobileSensingSuite
 #SBATCH --gres=gpu:1
 
-#export PATH=$PATH:/gscratch/bdata/mikeam/anaconda3/bin
 source ~/.bashrc
-echo "submiting"
 conda activate MobileSensingSuite
-wandb agent $1 
-
+wandb agent $1 --count 1 
