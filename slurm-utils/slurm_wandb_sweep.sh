f949111df28980ee48c37f1cc3c1e07d43a6a9c2
TheMikeMerrill <mikeam@cs.washington.edu>
2022-06-02 15:10:01 -0700

    cpus per gpu

--- slurm-utils/slurm_wandb_sweep.sh
+++ slurm-utils/slurm_wandb_sweep.sh
@@ -7,7 +7,7 @@
 #SBATCH --time=4:00:00
 #SBATCH --nodes=1
 #SBATCH --mem=45G
-#SBATCH --cpus=4
+#SBATCH --cpus-per-gpu=4
 #SBATCH --ntasks-per-node=1
 #SBATCH --chdir=/gscratch/bdata/mikeam/MobileSensingSuite
 #SBATCH --gres=gpu:1
