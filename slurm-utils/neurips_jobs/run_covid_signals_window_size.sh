3e62b11d4a126983e88a3105e251193c1c2cf5f6
Mike Merrill <mikeam@cs.washington.edu>
2022-06-02 10:40:20 -0700

    Correct list syntax

--- slurm-utils/neurips_jobs/run_covid_signals_window_size.sh
+++ slurm-utils/neurips_jobs/run_covid_signals_window_size.sh
@@ -4,13 +4,13 @@ EVAL_PATH="/mmfs1/gscratch/bdata/mikeam/MobileSensingSuite/data/processed/COVID_
 BASE_COMMAND="--config configs/tasks/CovidSignalsPositivity.yaml --config configs/models/CNNToTransformerClassifier.yaml --model.dropout_rate 0.4  --model.learning_rate 0.003  --data.train_path $TRAIN_PATH --data.val_path $EVAL_PATH --model.batch_size 800 --trainer.check_val_every_n_epoch 1 --trainer.max_epochs 50 --trainer.log_every_n_steps 50 --early_stopping_patience 40 --model.warmup_steps 100"
 
 TASK_ARGS=(
-    "--data.window_onset_min 1 --data.window_onset_max 2",
-    "--data.window_onset_min 1 --data.window_onset_max 3",
-    "--data.window_onset_min 1 --data.window_onset_max 3",
-    "--data.window_onset_max 2",
-    "--data.window_onset_max 3",
-    "--data.window_onset_max 4",
-    "--data.window_onset_max 5",
+    "--data.window_onset_min 1 --data.window_onset_max 2"
+    "--data.window_onset_min 1 --data.window_onset_max 3"
+    "--data.window_onset_min 1 --data.window_onset_max 3"
+    "--data.window_onset_max 2"
+    "--data.window_onset_max 3"
+    "--data.window_onset_max 4"
+    "--data.window_onset_max 5"
 )
 for i in ${!TASK_ARGS[*]}; 
   do
