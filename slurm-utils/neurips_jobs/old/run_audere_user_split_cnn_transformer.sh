f81c8ee49a59f0a32c07ae9c7ee02072e4b0bc37
Esteban <safranchik@esteban.lan>
2022-12-02 11:17:12 -0800

    resotred old cnn-transformer script

--- slurm-utils/neurips_jobs/old/run_audere_user_split_cnn_transformer.sh
+++ slurm-utils/neurips_jobs/old/run_audere_user_split_cnn_transformer.sh
@@ -1,11 +1,7 @@
-TRAIN_PATH="/gscratch/bdata/estebans/Homekit2020/data/processed/split/audere_split_2020_02_10/train_7_day"
-EVAL_PATH="/gscratch/bdata/estebans/Homekit2020/data/processed/split/audere_split_2020_02_10/eval_7_day"
-TEST_PATH="/gscratch/bdata/estebans/Homekit2020/data/processed/split/audere_split_2020_02_10/test_7_day"
-
-
-BASE_COMMAND="--config configs/models/CNNToTransformerClassifier.yaml --model.learning_rate 0.003 --data.test_path $TEST_PATH --data.train_path $TRAIN_PATH --data.val_path $EVAL_PATH --model.batch_size 800 --trainer.check_val_every_n_epoch 1 --trainer.max_epochs 50 --trainer.log_every_n_steps 50 --early_stopping_patience 10 --checkpoint_metric  val/roc_auc"
-
-wandb artifact cache cleanup 1GB
+TRAIN_PATH="/mmfs1/gscratch/bdata/mikeam/MobileSensingSuite/data/processed/audere/audere_split_2020_02_10_to_2020_04_01_by_participant/train_7_day_no_scale"
+EVAL_PATH="/mmfs1/gscratch/bdata/mikeam/MobileSensingSuite/data/processed/audere/audere_split_2020_02_10_to_2020_04_01_by_participant/eval_7_day_no_scale"
+TEST_PATH="/mmfs1/gscratch/bdata/mikeam/MobileSensingSuite/data/processed/audere/audere_split_2020_02_10_to_2020_04_01_by_participant/test_7_day_no_scale"
+BASE_COMMAND="--config configs/models/CNNToTransformerClassifier.yaml --model.dropout_rate 0.4  --model.learning_rate 0.003  --data.test_path $TEST_PATH --data.train_path $TRAIN_PATH --data.val_path $EVAL_PATH --model.batch_size 800 --trainer.check_val_every_n_epoch 1 --trainer.max_epochs 50 --trainer.log_every_n_steps 50 --early_stopping_patience 40 --model.warmup_steps 100"
 
 TASKS=(
     "HomekitPredictFluPos"
@@ -15,15 +11,9 @@ TASKS=(
     "HomekitPredictFatigue"
 )
 
-SEEDS=(0 1 2 3 4)
-
 
 for i in ${!TASKS[*]};
   do
-    for seed in ${!SEEDS[*]};
-    do
-      pythonCommand="python src/models/train.py fit --config configs/tasks/${TASKS[$i]}.yaml ${BASE_COMMAND} --run_name ${TASKS[$i]}-seed_${seed}-$(date +%F) --notes 'Temporal Split'"
-#      eval $pythonCommand
-      eval "python slurm-utils/launch_on_slurm.py  -n 1 -m '48G' --num-gpus 1 -p gpu-rtx6k --num-cpus 5 --dir . --exp-name CNNTransformer-${TASKS[$i]}-temp_split-seed_${seed} --command \"$pythonCommand\" --conda-env \"mobs\""
-    done
+    pythonCommand="python src/models/train.py fit --config configs/tasks/${TASKS[$i]}.yaml $BASE_COMMAND --notes 'User Split'"
+    eval "python slurm-utils/launch_on_slurm.py  -n 1 -m '64G' --num-gpus 1 --num-cpus 5 --dir . --exp-name Split-CNNTransformer-${TASKS[$i]} --command \"$pythonCommand\""
   done
